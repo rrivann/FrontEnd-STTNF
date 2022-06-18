6ce15ee6e244b9d34f20200dfbd438d20c7f5c85
Rivan Albani Ray <rivanalbaniray88@gmail.com>
2022-06-18 20:35:51 +0700

    up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
 import {Provider} from 'react-redux';
 import store from './redux/store';
@@ -10,9 +10,9 @@ import store from './redux/store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
